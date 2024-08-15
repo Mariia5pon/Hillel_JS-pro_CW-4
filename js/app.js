6a8702bd4516f8a15bb85c7acc57acc1e521b7b7
mariia <mariia@mail.uni-paderborn.de>
2024-08-15 10:19:57 +0200

    add files

--- js/app.js
+++ js/app.js
@@ -1,55 +1,119 @@
-function checkType() {
-    alert('Подивись в консолі (F12).')
-    let stringType = "Hello!";
-    let numberType = 42;
-    let bigintType = 1234567890123456789012345678901234567890n;
-    let booleanType = true;
-    let undefinedType;
-    let nullType = null;
-    let objectType = {name: "John", age: 30};
-    let symbolType = Symbol("id");
-    let functionType = function myFunction() {
-        console.log("It's a function.");
-    };
-    let arrayType = [1, 2, 3]
-
-    console.log('Примітивні типи даних:');
-    console.log('* тип даних string - ' + typeof stringType);
-    console.log('* тип даних number - ' + typeof numberType);
-    console.log('* тип даних bigInt - ' + typeof bigintType);
-    console.log('* тип даних boolean - ' + typeof booleanType);
-    console.log('* тип даних undefined - ' + typeof undefinedType);
-    console.log('* тип даних null - ' + typeof nullType);
-    console.log('* тип даних symbol - ' + typeof symbolType);
-
-    console.log('Об`єктні типи даних:');
-    console.log('* тип даних object - ' + typeof objectType);
-    console.log('* тип даних function - ' + typeof functionType);
-    console.log('* тип даних array - ' + typeof arrayType);
+function switchABC() {
+    let number = parseInt(prompt('Enter your number please'));
+    switch (number) {
+        case 1:
+            console.log('a');
+            break;
+        case 2:
+            console.log('b');
+            break;
+        case 3:
+            console.log('c');
+            break;
+        default:
+            console.log('UA');
+    }
 }
 
-function displayStrings() {
+function biggestNumber() {
     const hidden = document.getElementById('hidden');
     hidden.hidden = !hidden.hidden;
 
-    const input1 = document.getElementById('input1').value;
-    const input2 = document.getElementById('input2').value;
-    const input3 = document.getElementById('input3').value;
+    const input1 = parseFloat (document.getElementById('input1').value);
+    const input2 = parseFloat(document.getElementById('input2').value);
 
-    const outputString = `${input3} - ${input1} - ${input2}`;
+    let outputNumber;
+
+    if (input1 > input2) {
+        outputNumber = `Більше число: ${input1}, менше число: ${input2}`;
+    } else if (input1 < input2) {
+        outputNumber = `Більше число: ${input2}, менше число: ${input1}`;
+    } else if (input1 === input2){
+        outputNumber = 'Числа рівні';
+    } else {
+        outputNumber = 'Введіть будь ласка числа';
+    }
 
     const outputField = document.getElementById('output');
-    outputField.textContent = outputString;
+    outputField.textContent = outputNumber;
 }
 
-function displayNumbers() {
-    const number = document.getElementById('input5').value;
+function compareNumbers() {
+    let number = parseInt(document.getElementById('input5').value);
 
-    let digit5 = Math.floor(number / 10000);
-    let digit4 = Math.floor((number % 10000) / 1000);
-    let digit3 = Math.floor((number % 1000) / 100);
-    let digit2 = Math.floor((number % 100) / 10);
-    let digit1 = number % 10;
+    if ((number <10 || number > 99) || (isNaN(number))){
+        alert('Введіть будь ласка двозначне число');
+        return;
+    }
 
-    alert(digit5 + " " + digit4 + " " + digit3 + " " + digit2 + " " + digit1);
+    let digit2 = number % 10;
+    number -= digit2;
+    number /= 10;
+    let digit1 = number;
+    let result;
+
+    if (digit1 > digit2) {
+        result = 'Перша цифра більше за другу.';
+    } else if (digit2 > digit1) {
+        result = 'Друга цифра більше за першу.';
+    } else {
+        result = 'Числа рівні';
+    }
+
+    alert(result);
 }
+
+function evenOrOdd() {
+    let number = parseInt(prompt('Enter your number please'));
+
+    if (isNaN(number)){
+        alert('Введіть будь ласка число');
+        return;
+    }
+
+    alert((number % 2 === 1) ? 'Число непарне' : 'Число парне');
+
+}
+
+function checkLastDigit() {
+    let number = parseInt(prompt('Enter your number please'));
+
+    if (isNaN(number)){
+        alert('Введіть будь ласка число');
+        return;
+    }
+
+    let lastDigit = number % 10;
+
+    alert(lastDigit + ' - ' + ((number % 2 === 1) ? 'число непарне' : 'число парне'));
+}
+
+function range() {
+    let number = parseInt(prompt('Enter your number please'));
+
+    if (isNaN(number)){
+        alert('Введіть будь ласка число');
+        return;
+    }
+
+    if (number>=0 && number <=500) {
+        alert('Входить');
+    } else {
+        alert('Не входить');
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
